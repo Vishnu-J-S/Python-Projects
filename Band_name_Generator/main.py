16a85211b9fbbb9eebf5dc1365fe80a4d3cefb8f
Vishnu-J-S <131599433+Vishnu-J-S@users.noreply.github.com>
2023-09-23 18:11:57 +0530

    Add files via upload

---
+++ Band_name_Generator/main.py
@@ -0,0 +1,7 @@
+
+print("Hello there!\n")
+city_name = input("Grow up,in which city,did you?\n")
+pet_name = input("Name of your pet,will you tell?\n")
+print("Meditate on this I must.....\n" +  city_name + pet_name)
+
+# Solution: https://replit.com/@appbrewery/band-name-generator-end
